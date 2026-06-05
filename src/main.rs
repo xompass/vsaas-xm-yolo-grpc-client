6bd3a181730d9fb1165b41349d11a4e03daf265b
daigrova <david.rojasv@hotmail.com>
2026-06-04 22:57:13 -0400

    feat: added 'isolate-license-plate' feature

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,11 @@ struct Opt {
     grpc_url: Vec<Url>,
     #[structopt(long, help = "how many inputs can be processed concurrently")]
     backpressure: usize,
+    #[structopt(
+        long,
+        help = "move license plate detections to dedicated sink 'isolated-license-plates'"
+    )]
+    isolate_license_plates: bool,
 }
 
 impl Opt {
@@ -163,6 +168,7 @@ async fn process_input(
     timeout: Duration,
     mut grpc: Grpc,
     writer: xedge::Writer,
+    isolate_license_plates: bool,
 ) {
     let jpg_bytes = JpgBytes(input.image().to_vec());
     let start = Instant::now();
@@ -170,7 +176,7 @@ async fn process_input(
         Ok(Ok(detections)) => {
             let request_t = start.elapsed();
             let post = Instant::now();
-            let transformed: Vec<_> = match detections
+            let mut transformed: Vec<_> = match detections
                 .into_iter()
                 .map(Datum::try_from_detection)
                 .collect::<Result<_, _>>()
@@ -181,6 +187,12 @@ async fn process_input(
                     return;
                 }
             };
+            let mut plate_detections = Vec::new();
+            if isolate_license_plates {
+                plate_detections = transformed
+                    .extract_if(.., |datum| &datum.class == "license_plate")
+                    .collect();
+            }
             let output = OutputPayload {
                 ts: input.json.ts.to_epoch(),
                 asset_id: &input.json.asset_id,
@@ -200,6 +212,25 @@ async fn process_input(
                     );
                 }
                 Err(e) => error!("output serialize: {e:?}"),
+            }
+            if !plate_detections.is_empty() {
+                let output = OutputPayload {
+                    ts: input.json.ts.to_epoch(),
+                    asset_id: &input.json.asset_id,
+                    // NOTE: backwards-compatible
+                    path: "",
+                    data: plate_detections,
+                };
+
+                match serde_json::to_vec(&output) {
+                    Ok(mut payload) => {
+                        payload.extend(input.image());
+                        if let Err(e) = writer.write("isolated-license-plates", &payload).await {
+                            error!("mqtt: {e:?}");
+                        }
+                    }
+                    Err(e) => error!("output serialize: {e:?}"),
+                }
             }
         }
         Ok(Err(grpc_error)) => error!("grpc: {grpc_error:?}"),
@@ -316,6 +347,7 @@ async fn main() {
                     Duration::from_secs(opt.grpc_timeout_s),
                     grpc.clone(),
                     module.writer(),
+                    opt.isolate_license_plates,
                 ));
                 running_tasks.push(task);
             }
